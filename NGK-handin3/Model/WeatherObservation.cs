ca2b3b73e6a933324d5fa54bba994d89208f3267
Lena Todnem <lena.todnem@gmail.com>
2020-04-20 14:49:51 +0200

    Model added

---
+++ NGK-handin3/Model/WeatherObservation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NGK_handin3.Model
+{
+    public class WeatherObservation
+    {
+        //Time
+        public DateTime Time { get; set; }
+
+        // Location
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        // Specifics about weather
+        public decimal Temperatue { get; set; }
+        public int Humidity { get; set; }
+        public decimal AirPressure { get; set; }
+
+
+
+    }
+}

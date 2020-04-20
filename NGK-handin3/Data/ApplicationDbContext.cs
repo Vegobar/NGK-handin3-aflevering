642c850d75de9af22ff97c6088dc2232b0bcbfa9
Lena Todnem <lena.todnem@gmail.com>
2020-04-20 14:27:18 +0200

    Db-setup

---
+++ NGK-handin3/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NGK_handin3.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+: base(options) { }
+
+
+        //Indsæt vores egne modeller
+
+
+    }
+}
